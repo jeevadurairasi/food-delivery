08c8a629c4cb57bd2d1c530f8d49a3b73c01d4dc
jeevadurairasi <96587645+jeevadurairasi@users.noreply.github.com>
2025-05-11 11:46:58 +0530

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -3,16 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component'; // Import HomeComponent
+
 const routes: Routes = [
-  { path: '', component: WelcomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: '**', redirectTo: '' }
+  { path: '', component: WelcomeComponent }, // Default route
+  { path: 'login', component: LoginComponent }, // Login route
+  { path: 'signup', component: SignupComponent }, // Signup route
+  { path: 'home', component: HomeComponent }, // Home route
+  { path: '**', redirectTo: '' } // Wildcard route (redirect to WelcomeComponent)
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-  
-}
+export class AppRoutingModule {}
--- app.module.ts
+++ app.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'; // Import AppRoutingModule
 import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    AppRoutingModule // Add AppRoutingModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
